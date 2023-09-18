65090d1c2a297f8646c6f41fde871a4834c6caac
Alpha <alalpha394@gmail.com>
2023-09-18 11:36:17 +0100

    git init

---
+++ index.js
@@ -0,0 +1,47 @@
+import prompt from "prompt-sync";
+
+let database = {};
+database.cours = [];
+database.profs = [];
+database.etudiants = [];
+
+//extraire les donnees dans l'objet table
+const cours = database.cours;
+const profs = database.profs;
+const etudiants = database.etudiants;
+cours.push({
+  titre: "Histoire",
+  description: "Ce cours est une introduction de lhistoire generale",
+  prof: "",
+  inscrits: [],
+});
+console.log("cours", cours);
+
+let prof = {
+  nom: "Babacar",
+  prenom: "SY",
+  email: "babacar1@gmail.com",
+  cours: "Histoire",
+};
+profs.push(prof);
+cours.prof = prof.nom;
+
+// demander de taper l'email de l'utilisateur
+let nouvoEmail = prompt()("taper le nouvel email: ");
+//demande de tapper le nouveau prenom et nom
+let nouvoPrenom = prompt()("taper le nouveau prenom: ");
+let nouvoNom = prompt()("taper le nouveau nom: ");
+
+let nouvoEtudiant = {
+  nom: nouvoNom,
+  prenom: nouvoPrenom,
+  email: nouvoEmail,
+};
+
+let inscrits = cours[cours.length - 1].inscrits;
+inscrits.push(nouvoEtudiant.email);
+etudiants.push(nouvoEtudiant);
+
+//afficher les donnees au terminal
+console.log("voici data apres modification(Objet)", JSON.stringify(database));
+console.log("voici data apres modification(Objet)", database);
